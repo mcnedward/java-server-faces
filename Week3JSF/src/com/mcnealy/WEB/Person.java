3fe519032358ab34974ab88215fc6c7512de58a8
Edward McNealy <emcnealy@isqft.com>
2014-05-29 17:07:12 -0400

    Week 3 - JSF Using JSF with XHTML.

---
+++ Week3JSF/src/com/mcnealy/WEB/Person.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.mcnealy.WEB;
+
+/**
+ * @author emcnealy
+ * 
+ */
+public class Person {
+	private String firstName = "Edward";
+	private String lastName = "McNealy";
+	private int age = 23;
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
